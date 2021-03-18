1f4a63a1121dec85eb6a58c28723da58d404f4f3
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-03-18 09:37:13 -0600

    Added comand line inputs

--- pdf-image/pdf_to_img.py
+++ pdf-image/pdf_to_img.py
@@ -1,21 +1,36 @@
 #  https://www.geeksforgeeks.org/convert-pdf-to-image-using-python/
 from pdf2image import convert_from_path
+import sys
 
-path = '/Users/britney/Documents/photo scans/texas 2009/_02.pdf'
-save_path = '/Users/britney/Documents/photo scans/texas 2009/'
+# read in parameters from terminal
+if len(sys.argv) < 5:
+    print('Must add comand line argunments')
+    print('Starting number, digits in name, original pdf, final folder path')
+    print('Digits 1-4, where 1 is x.jpg, 2 is xx.jpg and so on')
+    sys.exit()
+
+start = int(sys.argv[1])
+digit = int(sys.argv[2])
+path = sys.argv[3]
+save_path = sys.argv[4]
+
+
+#path = '/Users/britney/Documents/photo scans/texas 2009/_02.pdf'
+#save_path = '/Users/britney/Documents/photo scans/texas 2009/'
 
 # starting number of image
-start = 44
+#start = 44
 
 # Store Pdf with convert_from_path function
 images = convert_from_path(path)
+print('Converting a {} page PDF to images'.format(len(images)))
 
-for i in range(0, len(images), 2):
+for i in range(0, len(images), 1):
     num = str(start)
     if len(num) == 1:
-        num = '{}{}'.format('00', num)
-    elif len(num) == 2:
         num = '{}{}'.format('0', num)
+    elif len(num) == 2:
+        num = '{}{}'.format('', num)
     # assume only goes to three digits
 
     # increment start
