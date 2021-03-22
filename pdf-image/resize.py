6503c7dbf56113edd6c195be38897f3402000b63
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-03-22 11:53:49 -0600

    Rotate image

--- pdf-image/resize.py
+++ pdf-image/resize.py
@@ -1,19 +1,66 @@
 import os
 import sys
+from datetime import datetime as dt
+from PIL import Image
+import numpy as np
+
 
 def resize4x6(path):
     # get all files in directory
-    pics = os.listdir(path)
+    imgs = os.listdir(path)
 
     # remove pictures starting with _
-    pics = [x for x in pics if not x.startswith('_')]
-    print(pics)
+    imgs = [x for x in imgs if not x.startswith('_') and not x.startswith('.')
+            and os.path.isfile(os.path.join(path,x))]
 
     # create folder to move old ones to
+    folder_name = 'old_' + str(dt.now())
+    folder_path = os.path.join(path, folder_name)
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
 
-    # pick if should resize to 6 or 4 side
+    # for each image
+    for img_name in imgs:
+        #print(img_name)
+        img_path = os.path.join(path, img_name)
 
-    # fill in rest with white space
+        # read in image size
+        img = Image.open(img_path)
+        width, height = img.size
+
+        # pick if should resize to 6 or 4 side
+        new_dims = []
+        if height > width:
+
+            img = img.rotate(90, expand=True)
+            width, height = img.size
+        # do 4 first then go for 6
+        # testing 4in (386 px)
+        x = int(width*386/height)
+        if x <= 576:
+            new_dims = [x, 386]
+
+        # testing 6in (576 px)
+        x = int(height*576/width)
+        if x <= 386:
+            new_dims = [576, x]
+
+        # resize the image
+        img = img.resize(new_dims)
+        #img.show()
+
+        # fill in rest with white space
+        # make a white image
+        white = Image.new('RGB', (576, 386), (255, 255, 255))
+
+        # fill in pixels of image that fit
+        white.paste(img, (0,0))
+        #white.show()
+        # tada!
+        new_img_name = '4x6_' + img_name
+        white.save(os.path.join(path, new_img_name))
+        new_img_path = os.path.join(path, folder_name, img_name)
+        os.rename(img_path, new_img_path)
 
 
 if __name__ == '__main__':
